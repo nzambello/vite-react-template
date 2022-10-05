5216cbe1697d337b3f3488134a1fc58bf34edc6a
nzambello <nzambello@protonmail.com>
2022-10-05 12:36:05 +0200

    Add test coverage

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,9 @@ export default defineConfig({
   test: {
     globals: true,
     include: ['src/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    coverage: {
+      reporter: ['text', 'json-summary', 'json']
+    },
     environment: 'jsdom',
     setupFiles: './src/vitest.setup.ts',
     // you might want to disable it, if you don't have tests that rely on CSS
